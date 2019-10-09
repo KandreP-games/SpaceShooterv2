0a7fc42c0649d099f8df2baa061c0ff7aace727b
enrique <enrique.tunity@gmail.com>
2019-10-09 13:49:13 +0200

    v091019.1
    Very few things, trying a new behaviour of enemies

--- Assets/_GameObjects/Scripts/Enemies/DummyEnemy.cs
+++ Assets/_GameObjects/Scripts/Enemies/DummyEnemy.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class DummyEnemy : MobileEnemy
 {
-    
+    private GameObject player;
+    private float distance;
+
+    private void ToFleeAway()
+    {
+       distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance < 5)
+        {
+
+        }
+    }
 }
