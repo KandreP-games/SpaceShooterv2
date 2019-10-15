87625292248293ca47796f918b058511f2d65f4d
kleber24021 <kleber24021@gmail.com>
2019-10-15 23:20:31 +0200

    v151019.2
    InsideOutside Behaviour & Space Building

---
+++ Assets/_GameObjects/Scripts/BioDomeBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BioDomeBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if (GameManager.isInside)
+        {
+            Physics.gravity = new Vector3(0, -9.8f, 0);
+        }else if(GameManager.isInside == false)
+        {
+            Physics.gravity = new Vector3(0, -4.4f, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            GameManager.isInside = !GameManager.isInside;
+        }
+    }
+
+
+}
---
+++ Assets/_GameObjects/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool isInside;
+    private int numberEnemies;
+    public int waveNumber = 1;
+    private void Start()
+    {
+        numberEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        GameManager.isInside = false;
+    }
+
+    private void Update()
+    {
+        StartCoroutine("EnemyCounter");
+    }
+
+    IEnumerator EnemyCounter()
+    {
+        numberEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if(numberEnemies <= 0)
+        {
+            waveNumber += 1;
+        }
+        yield return null;
+    }
+}
